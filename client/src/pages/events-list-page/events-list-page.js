dc018154801c9b2594765f7fa7327d8308f87e30
Stefan Butler <stefanbutler@icloud.com>
2024-11-08 10:50:05 +0000

    feat: loading spinner

--- client/src/pages/events-list-page/events-list-page.js
+++ client/src/pages/events-list-page/events-list-page.js
@@ -2,16 +2,19 @@ import React, { useContext, useEffect, useState } from "react";
 import './events-list-page.css';
 import EventCard from "../../components/event-card/event-card";
 import { LocationContext } from "../../context/location-context";
+import ScaleLoader from "react-spinners/ScaleLoader";
 // events map import 
 
 function EventsListPage() {
   const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { location } = useContext(LocationContext);
 
   //fetch events from backend
   useEffect(() => {
     const fetchEvents = async () => {
       try {
+        setLoading(true);
         const response = await fetch(`http://localhost:3001/api/events?lat=${location.latitude}&long=${location.longitude}`);
         const data = await response.json();
         console.log(data.events);
@@ -19,31 +22,35 @@ function EventsListPage() {
         
       } catch (error) {
         console.error('error fetching events:  ', error);
+      } finally {
+        setLoading(false);
       }
     }
     if (location.latitude && location.longitude) fetchEvents();
-
   }, [location]);
 
 
   //add to favourites
 
   return (
-    //NAVBAR
-    //implement loading spinner while fetching data 
-      <div className="events-list-page">
-      <div className="events-list-container"> 
-        {events.length > 0 ? (
-          events.map((event) => (
-            <EventCard key={event.id} event={event} /> // Render EventCard for each event
-          ))
-        ) : (
-          <p>No events found.</p>
-        )}
-      </div>
+    // NAVBAR
+    <div className="events-list-page">
+      {loading ? (
+        <ScaleLoader />
+      ) : (
+        <div className="events-list-container">
+          {events.length > 0 ? (
+            events.map((event) => (
+              <EventCard key={event.id} event={event} /> // Render EventCard for each event
+            ))
+          ) : (
+            <p>No events found.</p>
+          )}
+        </div>
+      )}
       {/* Insert map component here */}
     </div>
-  )
-}
+  );
+        }
 
 export default EventsListPage;
